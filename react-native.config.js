0cffdc9cc4a7a4e5f2040640643f987e6b5910a8
Lonmee <Lonmee@126.com>
2023-02-24 19:32:29 +0800

    ^ android

--- react-native.config.js
+++ react-native.config.js
@@ -4,13 +4,13 @@ module.exports = {
       sourceDir: './ios',
     },
   },
-  dependencies: {
-    'nodejs-mobile-react-native': {
-      platforms: {
-        // Ignored because we need to set this up manually in order to
-        // call some APIs of this library directly in our MainActivity.java
-        android: null,
-      },
-    },
-  },
+  // dependencies: {
+  //   'nodejs-mobile-react-native': {
+  //     platforms: {
+  //       // Ignored because we need to set this up manually in order to
+  //       // call some APIs of this library directly in our MainActivity.java
+  //       android: null,
+  //     },
+  //   },
+  // },
 };
